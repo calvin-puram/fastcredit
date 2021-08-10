7ed1dd444a87d95395265e0329db4f66629fa9c3
calvin <puram.calvin@gmail.com>
2021-08-10 15:55:23 +0100

    add jwt test

--- server/test/helper.js
+++ server/test/helper.js
@@ -4,15 +4,15 @@ import Helper from "../helper/helper";
 describe("JWT, BYCRYPT", () => {
   let result;
   it("Should return hashed password (string)", () => {
-    result = Helper.hashPassword("Kazeem27");
+    result = Helper.hashPassword("2begood4");
     expect(result).to.be.a("string");
   });
   it("Should return true if password matches hashed", () => {
-    result = Helper.comparePassword(`${result}`, "Kazeem27");
+    result = Helper.comparePassword(`${result}`, "2begood4");
     expect(result).to.be.equal(true);
   });
   it("Should return false if password does not matches hashed", () => {
-    result = Helper.comparePassword(`${result}`, "Kazeem20");
+    result = Helper.comparePassword(`${result}`, "2begood5");
     expect(result).to.be.equal(false);
   });
 });
