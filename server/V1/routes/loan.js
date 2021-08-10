43517414e50d83bacdc105c7115377e3f342ceb7
calvin <puram.calvin@gmail.com>
2021-08-10 15:41:51 +0100

    add loan functionality

---
+++ server/V1/routes/loan.js
@@ -0,0 +1,26 @@
+import express from "express";
+import loanController from "../controller/loan";
+import Auth from "../middleware/Auth";
+import rows from "../rowHelper";
+
+const router = express.Router();
+
+// Create a loan application
+router.post(
+  "/loans",
+  Auth.verifyToken,
+  rows.checkLoan,
+  loanController.postLoan
+);
+
+// get all particular loan history
+router.get("/loans/history", Auth.verifyToken, loanController.loanHistory);
+
+// View loan repayment history
+router.get(
+  "/loans/:id/repayments",
+  Auth.verifyToken,
+  loanController.repaymentHistory
+);
+
+module.exports = router;
