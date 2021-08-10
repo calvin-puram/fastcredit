d6d80e44e94162e3f0ac44f9eb0278b9e6802aa8
calvin <puram.calvin@gmail.com>
2021-08-10 15:52:02 +0100

    add admin test

---
+++ server/V1/app.js
@@ -0,0 +1,66 @@
+import http from "http";
+import morgan from "morgan";
+import express from "express";
+import bodyparser from "body-parser";
+import path from "path";
+import swaggerUI from "swagger-ui-express";
+import user from "./v1/routes/user";
+import loan from "./v1/routes/loan";
+import admin from "./v1/routes/admin";
+import docs from "../swagger.json";
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+
+// API routes
+app.use(express.static(path.join("UI")));
+app.use("/api/v1/auth", user);
+app.use("/api/v1", loan);
+app.use("/api/v1", admin);
+
+// Home page route
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: 200,
+    data: [
+      {
+        message: "Welcome to Fast Credit Home Route",
+      },
+    ],
+  });
+});
+
+// Render quick credit documentation
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(docs));
+
+// handle all error
+app.use((err, req, res, next) => {
+  if (err) {
+    return res.status(500).json({
+      status: 500,
+      error: "Internal server error",
+    });
+  }
+  return next();
+});
+
+// Handle non exist route with with proper message
+app.use((req, res) => {
+  res.status(404).json({
+    status: 404,
+    error: "Wrong request. Route does not exist",
+  });
+});
+
+const server = http.createServer(app);
+const port = process.env.PORT || 5500;
+
+server.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`listening to server on 127.0.0.1:${port}`);
+});
+
+module.exports = app;
---
+++ server/test/admin.js
@@ -0,0 +1,280 @@
+import request from "supertest";
+import chai from "chai";
+import faker from "faker";
+import app from "../app";
+
+const { expect } = chai;
+
+let Token;
+let adminToken;
+let mail;
+// let loanId;
+let userid;
+
+describe("Admin Route version one", () => {
+  it("should login admin successfully and generate admin token", (done) => {
+    request(app)
+      .post("/api/v1/auth/signin")
+      .send({ email: "admin@quickcreditapp.com", password: "Kazeem27" })
+      .end((err, res) => {
+        const { body } = res;
+        adminToken = body.data[0].token;
+        done();
+      });
+  });
+  it("should login user successfully and generate user token", (done) => {
+    request(app)
+      .post("/api/v1/auth/signin")
+      .send({ email: "kazmobileapp@gmail.com", password: "Kazeem27" })
+      .end((err, res) => {
+        const { body } = res;
+        Token = body.data[0].token;
+        done();
+      });
+  });
+  it("should successfully signup a user with valid details", (done) => {
+    request(app)
+      .post("/api/v1/auth/signup")
+      .send({
+        email: faker.internet.email(),
+        firstName: "tester",
+        lastName: "testing",
+        password: "Kazeem27",
+        address: "27, tunji Olaiya street",
+      })
+      .end((err, res) => {
+        const { body } = res;
+        mail = body.data.email;
+        userid = body.data.id;
+        done();
+      });
+  });
+  it("should verified a user successfully", (done) => {
+    request(app)
+      .patch(`/api/v1/users/${mail}/verify`)
+      .set("token", adminToken)
+      .send({ status: "verified" })
+      .end((err, res) => {
+        const { body } = res;
+        expect(res.status).to.be.equal(200);
+        expect(body).to.have.property("data");
+        expect(body.data[0]).to.haveOwnProperty("message");
+        expect(body.data[0].message).to.be.equal(
+          `users with id:${userid} has been verified`
+        );
+        done();
+      });
+  });
+  it("should not verified a user successfully if not found", (done) => {
+    request(app)
+      .patch("/api/v1/users/kazmobileap@gmail.com/verify")
+      .set("token", adminToken)
+      .send({ status: "verified" })
+      .end((err, res) => {
+        const { body } = res;
+        expect(res.status).to.be.equal(404);
+        expect(body).to.have.property("error");
+        expect(body.error).to.be.equal("Not Found");
+        done();
+      });
+  });
+  it("should not authorize a user without a token", (done) => {
+    request(app)
+      .patch(`/api/v1/users/${mail}/verify`)
+      .send({ status: "verified" })
+      .end((err, res) => {
+        const { body } = res;
+        expect(res.status).to.be.equal(403);
+        expect(body).to.have.property("error");
+        expect(body.error).to.be.equal("Unauthorized!, you have to login");
+        done();
+      });
+  });
+  it("should not authorize a user in admin route", (done) => {
+    request(app)
+      .patch(`/api/v1/users/${mail}/verify`)
+      .set("token", Token)
+      .send({ status: "verified" })
+      .end((err, res) => {
+        const { body } = res;
+        expect(res.status).to.be.equal(403);
+        expect(body).to.have.property("error");
+        expect(body.error).to.be.equal("Unauthorized!, Admin only route");
+        done();
+      });
+  });
+  it("should not verified a user successfully with wrong input", (done) => {
+    request(app)
+      .patch(`/api/v1/users/${mail}/verify`)
+      .set("token", adminToken)
+      .send({ status: "verify" })
+      .end((err, res) => {
+        const { body } = res;
+        expect(res.status).to.be.equal(400);
+        expect(body).to.have.property("error");
+        done();
+      });
+  });
+  it("should post a user loan successfully", (done) => {
+    request(app)
+      .post("/api/v1/loans")
+      .set("token", Token)
+      .send({ amount: 2000.567, tenor: 2 })
+      .end((err, res) => {
+        expect(res.status).to.be.a("number");
+        done();
+      });
+  });
+  it("should get all loan application successfully", (done) => {
+    request(app)
+      .get("/api/v1/loans")
+      .set("token", adminToken)
+      .end((err, res) => {
+        const { body } = res;
+        expect(res.status).to.be.equal(200);
+        expect(body).to.have.property("data");
+        expect(body.data[0]).to.haveOwnProperty("message");
+        expect(body.data[0].message).to.be.equal("loans retrieve successfully");
+        done();
+      });
+  });
+  it("should get a specific loan application successfully", (done) => {
+    request(app)
+      .get("/api/v1/loans/1")
+      .set("token", adminToken)
+      .end((err, res) => {
+        const { body } = res;
+        expect(res.status).to.be.equal(200);
+        expect(body).to.have.property("data");
+        expect(body.data[0]).to.haveOwnProperty("message");
+        done();
+      });
+  });
+  it("should not get a specific loan application successfully", (done) => {
+    request(app)
+      .get("/api/v1/loans/678954")
+      .set("token", adminToken)
+      .end((err, res) => {
+        const { body } = res;
+        expect(res.status).to.be.equal(404);
+        expect(body).to.have.property("error");
+        done();
+      });
+  });
+  it("should view current loans (not fully repaid). successfully", (done) => {
+    request(app)
+      .get("/api/v1/loans?status=approved&repaid=false")
+      .set("token", adminToken)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(200);
+        expect(res.status).to.be.a("number");
+        done();
+      });
+  });
+  it("should view all paid loans successfully", (done) => {
+    request(app)
+      .get("/api/v1/loans?status=approved&repaid=true")
+      .set("token", adminToken)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(200);
+        expect(res.status).to.be.a("number");
+        done();
+      });
+  });
+  it("should not approve or reject a loan without correct input", (done) => {
+    request(app)
+      .patch("/api/v1/loans/1")
+      .set("token", adminToken)
+      .send({ status: "approve" })
+      .end((err, res) => {
+        const { body } = res;
+        expect(res.status).to.be.equal(400);
+        expect(body).to.have.property("error");
+        done();
+      });
+  });
+  it("should not approve a loan  successfully", (done) => {
+    request(app)
+      .patch("/api/v1/loans/4")
+      .set("token", adminToken)
+      .send({ status: "approved" })
+      .end((err, res) => {
+        const { body } = res;
+        expect(res.status).to.be.equal(400);
+        expect(body).to.have.property("error");
+        done();
+      });
+  });
+  it("should approve or reject a loan  successfully", (done) => {
+    request(app)
+      .patch("/api/v1/loans/1")
+      .set("token", adminToken)
+      .send({ status: "approved" })
+      .end((err, res) => {
+        const { body } = res;
+        expect(res.status).to.be.equal(200);
+        expect(body).to.have.property("data");
+        expect(body.data[0]).to.haveOwnProperty("message");
+        done();
+      });
+  });
+  it("should successfully post loan repayment for a client", (done) => {
+    request(app)
+      .post("/api/v1/loans/1/repayment")
+      .send({ paidAmount: 5 })
+      .set("token", adminToken)
+      .end((err, res) => {
+        const { body } = res;
+        expect(res.status).to.be.equal(201);
+        expect(body).to.have.property("data");
+        done();
+      });
+  });
+  it("should not post loan repayment for a client if repayment is more than loan", (done) => {
+    request(app)
+      .post("/api/v1/loans/1/repayment")
+      .send({ paidAmount: 400000 })
+      .set("token", adminToken)
+      .end((err, res) => {
+        const { body } = res;
+        expect(res.status).to.be.equal(400);
+        expect(body).to.have.property("error");
+        expect(body.error).to.be.equal("You can not pay more than your debt!");
+        done();
+      });
+  });
+  it("should not post loan repayment for a client", (done) => {
+    request(app)
+      .post("/api/v1/loans/1/repayment")
+      .send({})
+      .set("token", adminToken)
+      .end((err, res) => {
+        const { body } = res;
+        expect(res.status).to.be.equal(400);
+        expect(body).to.have.property("error");
+        done();
+      });
+  });
+  it("should not post loan repayment for a client", (done) => {
+    request(app)
+      .post("/api/v1/loans/2000/repayment")
+      .send({ paidAmount: 1000 })
+      .set("token", adminToken)
+      .end((err, res) => {
+        const { body } = res;
+        expect(res.status).to.be.equal(404);
+        expect(body).to.have.property("error");
+        expect(body.error).to.be.equal("No such loan found");
+        done();
+      });
+  });
+  it("should delete a user successfully", (done) => {
+    request(app)
+      .delete(`/api/v1/auth/user/${userid}`)
+      .set("token", adminToken)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(200);
+        done();
+      });
+  });
+});
